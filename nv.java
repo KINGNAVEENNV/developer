8c26b8b665575363ef52a15e03e7515d162c9eb1
KINGNAVEENNV <132225555+KINGNAVEENNV@users.noreply.github.com>
2023-05-09 22:38:36 +0530

    Update nv.java

--- nv.java
+++ nv.java
@@ -2,6 +2,6 @@ import java.util.*;
 public class Main{
     public static void main(String[]args)
     {
-        System.out.println("hello nv");
+        System.out.println("love u chellam");
     }
 }
